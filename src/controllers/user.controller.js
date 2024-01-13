d8ef7481769607e67877883f72bc1fefd2d00874
SAI <sai552kk@gmail.com>
2024-01-13 16:20:58 +0530

    fixing the filepath for the multer middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -60,15 +60,15 @@ const registerUser = asyncHandler( async (req, res) => {
 
     if(userExisted) throw new ApiError(409, "User with email or username already exists");
 
-    const avatarLocalPath = req.files?.avatar[0]?.path
+    const avatarLocalPath = req.files?.avatar?.[0]?.path
     console.log(req.files)
     console.log(req.body)
     console.log(userExisted)
 
-    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
     let coverImageLocalPath;
     if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-        coverImageLocalPath: req.files.coverImage[0].path
+        coverImageLocalPath = req.files.coverImage[0].path
     }
 
     if (!avatarLocalPath) {
@@ -76,7 +76,7 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath) 
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath)   
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if(!avatar) throw new ApiError(400, "Avatar file is required");
 
